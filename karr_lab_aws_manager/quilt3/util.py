7596b3c7a19aafd6f6926719327c21d77c43a17e
Zhouyang Lian <zhouyang.lian@familian.life>
2019-09-28 23:37:08 -0400

    quilt3 stuff

--- karr_lab_aws_manager/quilt3/util.py
+++ karr_lab_aws_manager/quilt3/util.py
@@ -7,3 +7,9 @@ class QuiltUtil:
         ''' Interacting with karrlab quilt3 bucket and packages
         '''
         pass
+
+    def package_datanator(self):
+        ''' Specifically used for uploading datanator package to
+            quilt3
+        '''
+        pass
