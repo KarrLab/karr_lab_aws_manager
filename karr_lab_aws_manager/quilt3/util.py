d5b8e03ac0405a3ffd1ddb3aed7693abceb19286
Zhouyang Lian <zhouyang.lian@familian.life>
2019-09-29 22:45:26 -0400

    quilt3 requires lower version of urllib3

--- karr_lab_aws_manager/quilt3/util.py
+++ karr_lab_aws_manager/quilt3/util.py
@@ -1,15 +1,36 @@
 import quilt3
+from karr_lab_aws_manager.s3 import util as s3_util
 
 
-class QuiltUtil:
+class QuiltUtil(s3_util.S3Util):
 
-    def __init__(self, catalog_homepage):
+    def __init__(self, credential_path=None, config_path=None, profile_name=None):
         ''' Interacting with karrlab quilt3 bucket and packages
         '''
-        pass
+        super().__init__(credential_path=credential_path, config_path=config_path, profile_name=profile_name)
+        self.package = quilt3.Package()
 
-    def package_datanator(self):
+    def add_to_package(self, source=None, destination=None, meta=None):
         ''' Specifically used for uploading datanator package to
             quilt3
+            Args:
+                source (:obj: `list` of :obj: `str`): sources to be added to package
+                destination (:obj: `list` of :obj: `str` ): package(s) to be manipulated
+                meta (:obj: `list` of :obj: `dict`): package meta
+            Return:
+
         '''
-        pass
+        length = len(source)
+        if not (all(len(lst)) == length for lst in [destination, meta]):
+            return 'All three entries must be lists of the same length.'
+        suffix = '/'
+        for i, s in enumerate(source):
+            d = destination[i]
+            m = meta[i]
+            if s.endswith(suffix) != d.endswith(suffix): # when s and d do not share the same suffix
+                return '{} and {} must have the same suffix. Operation stopped at {}th element.'.format(s, d, i)
+
+            if s.endswith(suffix):
+                self.package.set_dir(s, d, meta=m)
+            else:
+                self.package.set(s, d, meta=m)
--- tests/quilt3/test_util.py
+++ tests/quilt3/test_util.py
@@ -1,1 +1,35 @@
 import unittest
+from karr_lab_aws_manager.quilt3 import util
+import tempfile
+import shutil
+from pathlib import Path
+
+
+
+class TestQuiltUtil(util.QuiltUtil):
+
+    @classmethod 
+    def setUpClass(cls):
+        cls.cache_dir_source = tempfile.mkdtemp()
+        cls.cache_dir_destination = tempfile.mkdtemp()
+        cls.src = util.QuiltUtil(credential_path='.wc/third_party/aws_credentials',
+                                 config_path='.wc/third_party/aws_config', profile_name='karrlab-zl')
+        cls.file = 'test_util.txt'
+        cls.test_file = cls.cache_dir_source + cls.file
+        Path(cls.test_file).touch()
+
+
+    @classmethod 
+    def tearDownClass(cls):
+        shutil.rmtree(cls.cache_dir_source)
+        shutil.rmtree(cls.cache_dir_destination)
+
+    def test_add_to_package(self):
+        source_0 = [cls.test_file, cls.cache_dir_source, cls.cache_dir_source]
+        destination_0 = [self.cache_dir_destination+cls.file,
+                         cls.cache_dir_destination, self.cache_dir_destination+cls.file]
+        meta_0 = [{'k_entry_0': 'v_entry_0'}, {
+            'k_entry_1': 'v_entry_1'}, {'k_entry_2': 'v_entry_2'}]
+        r_0 = self.src.add_to_package(source_0, destination_0, meta_0)
+        self.assertEqual(
+            r_0, '{} and {} must have the same suffix. Operation stopped at {}th element.'.format(source_0[2], destination_0[2], meta_0[2]))
