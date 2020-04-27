ba2b8b8b813d20f3c27cf868b5e6093b69bed4bb
Jonathan Karr <jonrkarr@gmail.com>
2020-04-27 15:16:35 -0400

    fixing setup script

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ md = pkg_utils.get_package_metadata(dirname, name)
 # install package
 setuptools.setup(
     name=name,
-    version=karr_lab_aws_manager._version.__version__,
+    version=md.version,
     description='Package to manage aws services run by karr lab',
     long_description=md.long_description,
     url="https://github.com/KarrLab/" + name,
